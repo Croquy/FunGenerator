7aca6df50668da4942a86f4b373a7cadb9daa746
Croquy <86916937+Croquy@users.noreply.github.com>
2026-03-12 12:13:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ function setGenerator(name, longname){
   document.getElementById('current-generator').textContent = longname;
   generate();
 }
-
+  function randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  function r(arr){
+    return arr[Math.floor(Math.random()*arr.length)];
+  }
 function generate(){
 
   let text = "";
